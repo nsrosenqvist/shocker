66d537e5cf863a8d6d488f4e5a613da897c5401c
Niklas Rosenqvist <niklas.s.rosenqvist@gmail.com>
2014-08-22 11:41:02 +0200

    pre-release

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
 Shocker
 =======
+
+Shocker is an utility to generate markdown formatted documentation from shell source files. The resulting files can easily be used to create an online api since it's compatible with the excellent [mkdocs](http://www.mkdocs.org/)! The generated markdown is "[GitHub flavoured](https://help.github.com/articles/github-flavored-markdown)", meaning that it supports GitHub's (and mkdocs) syntax highlighting and table definitions.
+
+Comments should be formatted in a DocBlocks syntax and can be either single line or multiline. The start and end of a comment that should be processed is marked by the following character combination: `#/`
+
+An example of a multiline comment:
+```bash
+#/
+# The first line is a short one line summary of the function
+#
+# The second line/paragraph is a longer description that IDE's
+# sometimes use for giving helpful tooltips. Here you can explain
+# the usage of the function much more detailed. As you can see
+# can all parts of the DocBlock, except the summary, consist of
+# multiple lines.
+#
+# @param string   $1   You can use as much spacing, both tabs and
+#                      space, to make the definitions more readable
+# @param int      $2   A second parameter which is defined as int
+# @return int          Either 0 or 1
+#
+# @author Niklas Rosenqvist
+# @other  If you don't want to write a summary or description can
+#         both be omitted without any issues.
+#/
+function example() {
+    ...
+}
+```
+
+Examples of singleline comments:
+```bash
+#/ Boy! That was a short summary #/
+function example() {
+    ...
+}
+
+#/ @author Niklas Rosenqvist #/
+function example2() {
+    ...
+}
+```
+
+The wiki is the result of Shocker running on it's own source file. [Check it out!](https://github.com/nsrosenqvist/shocker/wiki)
+
+## Installation
+
+Clone the repository and then use make.
+
+```bash
+git clone https://github.com/nsrosenqvist/quickscript.git && cd quickscript
+make && sudo make install
+```
+
+## Usage
+
+The argument you provide can either be a directory containing shell files or a single file. The program's output is a bit different depending on what you specify.
+
+Single file example:
+```bash
+shocker -x 1 -c "Copyright © 2014, Niklas Rosenqvist" -o documentation.md input-file.sh
+```
+
+In the above example the input was a single source file. By default the output would have been written to "input-file.md" but we specifically told it to output it to "documentation.md". The -x flag made the resulting file's heading to be "input-file" instead of "input-file.sh", it removed the extension from the file name (-t could be used instead to set a custom heading). The flag -c set a copyright statement to the bottom of the file.
+
+Directory example:
+```bash
+shocker -Tx 1 -c "Copyright © 2014, Niklas Rosenqvist" -t "API" -o docs dir/input-dir
+```
+
+This time we process all the files in the directory "dir/input-dir" and output it to "docs/". We've set the flag -T (uppercase) which generates a table of contents file in the directory above the output directory. Now the -t (lowercase) sets the heading of the Table of contents file instead since we can't set the same heading for each and every file, it wouldn't make sense. A project that uses this for it's documentation is the QuickScript framework - refer to [their Makefile](https://github.com/nsrosenqvist/quickscript/blob/master/Makefile) for more information.
+
+### Parameters
+
+Parameter | Explanation
+--------- | -----------
+-t        | Set the file heading
+-f        | Allow file overwrites. If not set the program will abort when it attempts to overwrite an existing file.
+-o        | Output file or directory
+-x        | "0" for including the filename's extension in the file heading and "1" (false) to exclude it.
+-c        | Specify a copyright statement that will be added to the end of each file.
+-T        | Create an index file with a table of contents, only applicable when processing directories.
+
+## Development
+
+Feel free to make pull requests with your own contributions. If you want to help out and don't know what to do you can try to tackle an issue from the [issue tracker](https://github.com/nsrosenqvist/shocker/issues).
+
+## Notice
+
+The program is licensed under LGPL v2.1, please refer to the LICENSE file for more information.
---
+++ shocker.sh
@@ -0,0 +1,540 @@
+#!/bin/bash
+
+#-----------------------------------------------------------------------------------------------------------------#
+# A script that parses DocBlocks formatted comments in shell scripts and outputs markdown formatted documentation
+# Copyright (C) 2014 Niklas Rosenqvist
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#-----------------------------------------------------------------------------------------------------------------#
+
+# Abort on errors
+set -e
+
+# Globals
+SHLANG="sh"
+TITLE=""
+OUTPUT=""
+EXTENSION=1
+FORCE=1
+COPYRIGHT=""
+ToC=1
+declare -A PARAMS=()
+declare -A PROPERTIES=()
+
+#/
+# Used to find out the longest table cell
+#
+# To get properly formatted tables, where all the cells are aligned,
+# we have to find out what the maximum length is. We do that by passing
+# both the old and new value to compare with and sets provided variable
+# by using eval. That way we can loop easily and find it quickly.
+#
+# @param int $1 The currently highest value
+# @param int $2 The new value to compare with
+# @param string $3 The name of the variable to set, we use the variable
+#                  name of the one we provide for $1.
+#
+# @author Niklas Rosenqvist
+#/
+function take_if_higher() {
+    local oldval=$1
+    local newval=$2
+    local saveto="$3"
+
+    if [ $newval -gt $oldval ]; then
+        eval "$saveto=$2"
+    fi
+}
+
+#/ Pads string so that tables are properly formatted #/
+function pad_string() {
+    printf "%-${2}s" "$1"
+}
+
+#/ Repeat a character multiple a user set of times #/
+function repeat_character() {
+    local str="$(printf "%-${2}s" "$1")"
+    echo "${str// /$1}"
+}
+
+#/ Simple sed command to remove trailing and leading whitespace #/
+function trim_whitespace() {
+    echo "$1" | sed -e 's/^[ \t]*//;s/[ \t]*$//'
+}
+
+#/ Removes multiple slashes, used only to get proper paths for the table of contents #/
+function strip_multi_slash() {
+    echo "$1" | sed 's#//*#/#g'
+}
+
+#/
+# A shorthand for writing to file
+#
+# It uses the global variable OUTPUT to determine where stdout should be directed
+#
+# @param string $1 The line that should be written
+# @author Niklas Rosenqvist
+#/
+function write() {
+    echo "$1" >> "$OUTPUT"
+}
+
+#/
+# Write's a DocBlock to the output file
+#
+# In addition to the parameters given, it also uses the global variables PARAMS and PROPERTIES,
+# from which the data is extracted from.
+#
+# @param string $1 The function name
+# @param string $2 The summary describing the function
+# @param string $3 The longer explanation giving more information about the function's usage
+# @return int It only returns 0 if no errors hindered the execution
+#
+# @author Niklas Rosenqvist
+#/
+function write_block() {
+    local funcname="$1"
+    local summary="$2"
+    local description="$3"
+
+    local tableparamskey=()
+    local tableparamsval=()
+    local maxkeylength=0
+    local maxvallength=0
+    local towrite=""
+    declare -A temparray=()
+
+    # Write what we've got
+    write "## $funcname()"
+    write ""
+    write "\`\`\`$SHLANG"
+    write "$funcname() "
+    write "\`\`\`"
+    write ""
+
+    if [ -n "$summary" ]; then
+        write "*$summary*"
+        write ""
+    fi
+
+    if [ -n "$description" ]; then
+        write "$description"
+        write ""
+    fi
+
+    if [ ${#PARAMS[@]} -gt 0 ]; then
+        # Separate the keys and their values into each their own array
+        for key in "${!PARAMS[@]}"; do
+            tableparamskey+=("${key#*:} (${key%%:*})")
+            tableparamsval+=("${PARAMS[$key]}")
+        done
+
+        # Get the longest lines so that we can pad and properly format tables
+        for key in "${tableparamskey[@]}"; do
+            take_if_higher $maxkeylength ${#key} "maxkeylength"
+        done
+        for val in "${tableparamsval[@]}"; do
+            take_if_higher $maxvallength ${#val} "maxvallength"
+        done
+
+        # Write out the parameters and their descriptions into a formatted table
+        # We loop from the back since the associative arrays are reversed
+        for ((i=${#PARAMS[@]}-1; i>=0; i--)); do
+            towrite="| $(pad_string "${tableparamskey[$i]}" $maxkeylength) |"
+            towrite+=" $(pad_string "${tableparamsval[$i]}" $maxvallength) |"
+            write "$towrite"
+        done
+
+        maxkeylength=0
+        maxvallength=0
+        write ""
+    fi
+
+    # Write out return type
+    if [ -n "${PROPERTIES[return]}" ]; then
+        line="${PROPERTIES[return]}"
+        local returnpropertytype="${line%% *}"
+        line="${line#* }"
+
+        write "**return ($returnpropertytype)** - $line"
+        write ""
+
+        # Remove return from array by recreating one
+        for key in "${!PROPERTIES[@]}"; do
+            if [ "$key" != "return" ]; then
+                temparray[$key]="${PROPERTIES[$key]}"
+            fi
+        done
+
+        PROPERTIES=()
+
+        for key in "${!temparray[@]}"; do
+            PROPERTIES[$key]="${temparray[$key]}"
+        done
+    fi
+
+    # Write the remaining properties in a list
+    local count=0
+    local proplength=${#PROPERTIES[@]}
+
+    if [ $proplength -gt 0 ]; then
+        towrite=""
+
+        for key in "${!PROPERTIES[@]}"; do
+            count=$(($count+1))
+
+            if [ $count -lt $proplength ]; then
+                towrite+="$key: ${PROPERTIES[$key]}, "
+            else
+                towrite+="$key: ${PROPERTIES[$key]}"
+            fi
+        done
+
+        write "*$towrite*"
+        write ""
+    fi
+
+    return 0
+}
+
+
+#/
+# Parses the file's comments to markdown
+#
+# The function writes the content to OUTPUT which can be set by the -o flag.
+# It can parse both single and multiline comments. A DocBlock comment is started
+# and ended with the "#/" character combination
+#
+# @param string $1 The file that will be processed
+# @param string $2 An optional file heading. If not set then it will be extracted from the file name
+# @return int It only returns 0 if no errors hindered the execution
+#
+# @author Niklas Rosenqvist
+#/
+function parse_file() {
+    local file="$1"
+    local lineno=0
+    local title=""
+    local parsingblock=1
+    local summary=""
+    local description=""
+    local property=1
+    local param=1
+    local lastproperty=""
+    local lineofblock=0
+    local singleline=1
+    local checkname=1
+    local funcname=""
+    local first=""
+    local second=""
+
+    # Write file heading
+    if [ -n "$2" ]; then
+        title="$2"
+    else
+        title="$(basename "$file")"
+
+        if [ $EXTENSION -ne 0 ]; then
+            title="${title%.*}"
+        fi
+    fi
+
+    write "$title"
+    write "$(repeat_character "=" ${#title})"
+    write ""
+
+    # Process the file
+    while IFS= read -r line; do
+        lineno=$(($lineno+1))
+
+        # Convert tabs to spaces and remove double spaces
+        line="${line//	/ }" # tabs
+        line="${line//  / }" # double spaces
+
+        # Trim trailing and leading whitespace from line
+        line="$(trim_whitespace "$line")"
+
+        # Get the shell script language for syntax highlighting from the first line of the file
+        if [ $lineno -eq 1 ] && [ "${line:0:2}" = "#!" ]; then
+            SHLANG="$(basename "${line:2}")"
+
+            # Only allow GitHub supported shells (sh/zsh/bash), if not one then default to "sh"
+            if ! [ "$SHLANG" = "bash" -o "$SHLANG" = "sh" -o "$SHLANG" = "zsh" ]; then
+                SHLANG="sh"
+            fi
+            continue
+        fi
+
+        # Check if this line contains the function name,
+        # if it does we start printing out what we've collected
+        if [ $checkname -eq 0 ] && [ -n "$line" ]; then
+            checkname=1
+
+            # Extract the function name
+            first="${line%% *}"
+            line="${line#* }"
+            second="${line%% *}"
+
+            if [ "$first" = "function" ]; then
+                funcname="${second%%()*}"
+            fi
+
+            # Write the DocBlock to the output file
+            write_block "$funcname" "$summary" "$description"
+            continue
+        fi
+
+        # If this is the end of a DocBlock we will start searching for the function definition
+        if [ "$line" = "#/" ] && [ $parsingblock -eq 0 ]; then
+            parsingblock=1
+            checkname=0
+            continue
+        fi
+
+        # Is this the start of a DocBlock? Reset variables and start parsing
+        if [[ "$line" == "#/"* ]] && [ $parsingblock -eq 1 ]; then
+            parsingblock=0
+            lineofblock=0
+            description=""
+            summary=""
+            funcname=""
+            property=1
+            PARAMS=()
+            PROPERTIES=()
+
+            # Is it a single line block?
+            if [ ${#line} -gt 2 ] && [ "${line:$((${#line}-2)):2}" = "#/" ]; then
+                # Change the line so that it's formatted as a regular DocBlock content line
+                line="# $(trim_whitespace "${line:2:$((${#line}-4))}")"
+                singleline=0
+            else
+                continue
+            fi
+        fi
+
+        # Are we parsing a comment block?
+        if [ $parsingblock -eq 0 ]; then
+            lineofblock=$(($lineofblock+1))
+            line="${line:2}"
+
+            # Only parse lines that aren't blank
+            if [ -n "$line" ]; then
+                # Parse property
+                if [ "${line:0:1}" = "@" ]; then
+                    lastproperty="${line:1}"
+                    lastproperty="${lastproperty%% *}"
+                    param=1
+                    property=0
+
+                    # Is the property a param?
+                    if [ "$lastproperty" = "param" ]; then
+                        param=0
+                        line="${line#* }"
+                        first="${line%% *}"
+                        line="${line#* }"
+                        second="${line%% *}"
+
+                        if [ "${first:0:1}" = "$" ]; then
+                            lastproperty="any:$first"
+                        else
+                            lastproperty="$first:$second"
+                            line="${line#* }"
+                        fi
+
+                        PARAMS["$lastproperty"]="$line"
+                    else
+                        PROPERTIES["$lastproperty"]="${line#* }"
+                    fi
+                else
+                    line="$(trim_whitespace "$line")"
+
+                    if [ $property -eq 0 ]; then
+                        if [ $param -eq 0 ]; then
+                            PARAMS["$lastproperty"]+=" $line"
+                        else
+                            PROPERTIES["$lastproperty"]+=" $line"
+                        fi
+                    else
+                        # If not a property then either a summary or description
+                        if [ $lineofblock -eq 1 ]; then
+                            summary="$line"
+                        else
+                            # Add a space if we're on a multiline description
+                            if [ -n "$description" ]; then
+                                description+=" "
+                            fi
+
+                            description+="$line"
+                        fi
+                    fi
+                fi
+            fi
+
+            # Is this the end of a single line block?
+            if [ $singleline -eq 0 ]; then
+                parsingblock=1
+                singleline=1
+                checkname=0
+            fi
+        fi
+    done < "$file"
+
+    # Append the copyright statement
+    if [ -n "$COPYRIGHT" ]; then
+        write "$(repeat_character "-" ${#COPYRIGHT})"
+        write "$COPYRIGHT"
+        write ""
+    fi
+
+    return 0
+}
+
+# Parse the options
+while getopts "t:fo:x:c:T" opt; do
+    case "$opt" in
+        t)
+            # Specify file heading
+            TITLE="$(trim_whitespace "$OPTARG")"
+        ;;
+        f)
+            # Set if you want to allow overwrites
+            FORCE=0
+        ;;
+        o)
+            # Specify output file/dir
+            OUTPUT="$OPTARG"
+        ;;
+        x)
+            # 1/0 whether the file extension should be shown in the file heading
+            EXTENSION=$OPTARG
+        ;;
+        c)
+            # Copyright statement
+            COPYRIGHT="$(trim_whitespace "$OPTARG")"
+        ;;
+        T)
+            # Create a Table Of Contents
+            ToC=0
+        ;;
+        \?)
+            echo "Invalid option: $OPTARG" 1>&2
+            echo "Aborting..."
+            exit 1
+        ;;
+    esac
+done
+
+shift $(($OPTIND-1))
+
+# If a directory is specified we parse a directory and create an index file with a table of contents
+if [ -d "$1" ]; then
+    # Set the output dirs
+    if [ -z "$OUTPUT" ]; then
+        OUTPUT="./"
+    fi
+
+    files_parsed=()
+    filesroot="$OUTPUT"
+    filesdir="$filesroot/api/"
+    mkdir -p "$filesdir"
+
+    # Loop through all files found in the directory
+    while IFS= read -r file; do
+        filename="$(basename "$file")"
+        filename="${filename%.*}.md"
+        OUTPUT="$filesdir/$filename"
+
+        # Abort if we have to overwrite a file and the forced flag is off
+        if [ -e "$OUTPUT" ]; then
+            if [ $FORCE -ne 0 ]; then
+                echo "The file \"$OUTPUT\" already exists. If you want to continue anyway and overwrite, specify the flag -f (force)." 1>&2
+                echo "Aborting..."
+                exit 1
+            else
+                rm -f "$OUTPUT"
+            fi
+        fi
+
+        # Parse the file and save a reference for the table of contents
+        parse_file "$file"
+        files_parsed+=("$OUTPUT")
+    done < <(find "$1" -maxdepth 1 -type f -iregex ".*\.\(sh\|bash\|zsh\)" | sort -V)
+
+    # Create the Table of Contents if set to do so
+    if [ $ToC -eq 0 ]; then
+        # Write file heading
+        OUTPUT="$filesroot/index.md"
+
+        # Abort if we have to overwrite a file and the forced flag is off
+        if [ -e "$OUTPUT" ]; then
+            if [ $FORCE -ne 0 ]; then
+                echo "The file \"$OUTPUT\" already exists. If you want to continue anyway and overwrite, specify the flag -f (force)." 1>&2
+                echo "Aborting..."
+                exit 1
+            else
+                rm -f "$OUTPUT"
+            fi
+        fi
+
+        # Set the file heading
+        if [ -n "$TITLE" ]; then
+            heading="$TITLE"
+        else
+            heading="Table of contents"
+        fi
+
+        write "$heading"
+        write "$(repeat_character "=" ${#heading})"
+        write ""
+
+        # Write the table of contents
+        for file in "${files_parsed[@]}"; do
+            title="$(basename "$file")"
+
+            if [ $EXTENSION -ne 0 ]; then
+                title="${title%.*}"
+            fi
+
+            write "- [$title]($(strip_multi_slash "${file/$filesroot\//}"))"
+        done
+    fi
+# Processing a single file
+else
+    # Set the output file
+    if [ -z "$OUTPUT" ]; then
+        OUTPUT="$(basename "$1")"
+        OUTPUT="${OUTPUT%.*}.md"
+    fi
+
+    # Abort if we have to overwrite a file and the forced flag is off
+    if [ -e "$OUTPUT" ]; then
+        if [ $FORCE -ne 0 ]; then
+            echo "The file \"$OUTPUT\" already exists. If you want to continue anyway and overwrite, specify the flag -f (force)." 1>&2
+            echo "Aborting..."
+            exit 1
+        else
+            rm -f "$OUTPUT"
+        fi
+    fi
+
+    # Parse the file
+    if [ -n "$TITLE" ]; then
+        parse_file "$1" "$TITLE"
+    else
+        parse_file "$1"
+    fi
+fi
+
+exit 0
